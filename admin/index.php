5d1bed8d5263ff60667c614dff7a0127a95e12dc
zil <azilanur965@gmail.com>
2024-09-26 08:23:06 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,152 +1,185 @@
-<?php
+<?php 
 session_start();
-$userid = $_SESSION['userid'];
-include_once '../config/koneksi.php';
+include '../config/koneksi.php';
 if ($_SESSION['status'] != 'login') {
-    echo "<script>
-    alert('Anda belum Login!');
-    location.href='../index.php';
-    </script>";
+  echo "<script>
+  alert('Anda Belum Login!');
+  location.href='../index.php';
+  </script>";
 }
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
-    <link rel="shortcut icon" type="image/jpg" href="../assets/Icon.jpg">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
-    <title>Website Galeri Foto</title>
-    <style>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Website Galeri Foto</title>
+  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+  <style>
     body {
-    background-image: url('../assets/img/bg3.jpg');
-    background-size: cover;}
-</style>
+      background-image: url('../assets/img/bg3.jpg');
+      background-size: cover;
+    }
+  </style>
 </head>
-
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container">
-            <a class="navbar-brand" href="index.php"> Website Galeri Foto</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
-                <div class="navbar-nav me-auto">
-                    <a href="home.php" class="nav-link">Home</a>
-                    <a href="album.php" class="nav-link">Album</a>
-                    <a href="poto.php" class="nav-link">Foto</a>
-                </div>
-                <a href="../config/aksi_logout.php" class="btn btn-outline-danger m-1">Keluar</a>
-            </div>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+        <div class="navbar-nav me-auto">
+          <a href="home.php" class="nav-link">Home</a>
+          <a href="album.php" class="nav-link">Album</a>
+          <a href="foto.php" class="nav-link">Foto</a>
         </div>
-    </nav>
+        <a href="../config/aksi_logout.php" class="btn btn-outline-danger m-1">Keluar</a>
+      </div>
+    </div>
+  </nav>
 
-    <div class="container mt-2">
-        <div class="row">
-            <?php
-            $query = mysqli_query($koneksi, "SELECT * FROM foto INNER JOIN user ON foto.userid=user.userid INNER JOIN album ON foto.albumid=album.albumid");
-            while ($data = mysqli_fetch_array($query)) {
-                ?>
-                <div class="col-md-3 mt-2">
-                    <a type="button" data-bs-toggle="modal" data-bs-target="#komentar<?php echo $data['fotoid'] ?>">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card mt-2">
+          <div class="card-header">Tambah Foto</div>
+          <div class="card-body">
+            <form action="../config/aksi_foto.php" method="POST" enctype="multipart/form-data">
+              <label class="form-label">Judul Foto</label>
+              <input type="text" name="judulfoto" class="form-control" required>
+              <label class="form-label">Deskripsi</label>
+              <textarea class="form-control" name="deskripsifoto" required></textarea>
+              <label class="form-label">Album</label>
+              <select class="form-control" name="albumid" required>
+                <?php 
+                $userid = $_SESSION['userid'];
+                $sql_album = mysqli_query($koneksi, "SELECT * FROM album WHERE userid='$userid'");
+                while($data_album = mysqli_fetch_array($sql_album)) { ?>
+                  <option value="<?php echo $data_album['albumid'] ?>"><?php echo $data_album['namaalbum'] ?></option>
+                <?php } ?>
+              </select>
+              <label class="form-label">File</label>
+              <input type="file" class="form-control" name="lokasifile" required>
+              <button type="submit" class="btn btn-primary mt-2" name="tambah">Tambah Data</button>
+            </form>
+          </div>
+        </div>
+      </div>
 
-                      <div class="card mb-2">
-                        <img src="../assets/img/<?php echo $data['lokasifile']; ?>"
-                        class="card-img-top"
-                        title="<?php echo $data['judulfoto']; ?>"
-                        style="height: 12rem;">
-                        <div class="card-footer text-center">
-                            <?php
-                            $fotoid = $data['fotoid'];
-                            $ceksuka = mysqli_query($koneksi, "SELECT * FROM likefoto WHERE fotoid='$fotoid' AND userid='$userid'");
-                            if (mysqli_num_rows($ceksuka) == 1) { ?>
-                                <a href="../config/proses_like.php?fotoid=<?php echo $data['fotoid'] ?>" type="submit" name="batalsuka"><i class="fa fa-heart"></i></a>
-                            <?php } else { ?>
-                                <a href="../config/proses_like.php?fotoid=<?php echo $data['fotoid'] ?>" type="submit" name="suka"><i class="fa-regular fa-heart"></i></a>
-                            <?php }
-                            $like = mysqli_query($koneksi, "SELECT * FROM likefoto WHERE fotoid='$fotoid'");
-                            echo mysqli_num_rows($like) . ' Suka';
-                            ?>
-                            <a href="#" type="button" data-bs-toggle="modal" data-bs-target="#komentar<?php echo $data['fotoid'] ?>"><i class="fa-regular fa-comment"></i></a> </a>
-                            <?php 
-                            $jmlkomen = mysqli_query($koneksi, "SELECT * FROM komentarfoto WHERE fotoid='$fotoid'");
-                            echo mysqli_num_rows($jmlkomen).' komentar';
-                            ?>
-                        </div>
-                    </div>
-                </a>
+      <div class="col-md-8">
+        <div class="card mt-2">
+          <div class="card-header">Data Album Foto</div>
+          <div class="card-body">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Foto</th>
+                  <th>Judul Foto</th>
+                  <th>Deskripsi</th>
+                  <th>Tanggal</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php 
+                $no = 1;
+                $userid = $_SESSION['userid'];
+                $sql = mysqli_query($koneksi, "SELECT * FROM foto WHERE userid='$userid'");
+                while($data = mysqli_fetch_array($sql)) { ?>
+                 <tr>
+                  <td><?php echo $no++ ?></td>
+                  <td><img src="../assets/img/<?php echo $data['lokasifile'] ?>" width="100"></td>
+                  <td><?php echo $data['judulfoto'] ?></td>
+                  <td><?php echo $data['deskripsifoto'] ?></td>
+                  <td><?php echo $data['tanggalunggah'] ?></td>
+                  <td>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit<?php echo $data['fotoid'] ?>">
+                      Edit
+                    </button>
 
-                <!-- Modal -->
-                <div class="modal fade" id="komentar<?php echo $data['fotoid'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog modal-xl">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <div class="row">
-                                <div class="col-md-8">
-                                    <img src="../assets/img/<?php echo $data['lokasifile']; ?>"
-                                    class="card-img-top"
-                                    title="<?php echo $data['judulfoto']; ?>">
-                                </div>
+                    <div class="modal fade" id="edit<?php echo $data['fotoid'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      <div class="modal-dialog">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Data</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                            <form action="../config/aksi_foto.php" method="POST" enctype="multipart/form-data">
+                              <input type="hidden" name="fotoid" value="<?php echo $data['fotoid'] ?>">
+                              <label class="form-label">Judul Foto</label>
+                              <input type="text" name="judulfoto" value="<?php echo $data['judulfoto'] ?>" class="form-control" required>
+                              <label class="form-label">Deskripsi</label>
+                              <textarea class="form-control" name="deskripsifoto" required><?php echo $data['deskripsifoto']; ?></textarea>
+                              <label class="form-label">Album</label>
+                              <select class="form-control" name="albumid">
+                                <?php 
+                                $userid = $_SESSION['userid'];
+                                $sql_album = mysqli_query($koneksi, "SELECT * FROM album WHERE userid='$userid'");
+                                while($data_album = mysqli_fetch_array($sql_album)) { ?>
+                                  <option <?php if($data_album['albumid'] == $data['albumid']) { ?> selected="selected" <?php } ?> value="<?php echo $data_album['albumid'] ?>"><?php echo $data_album['namaalbum'] ?></option>
+                                <?php } ?>
+                              </select>
+                              <label class="form-label">Foto</label>
+                              <div class="row">
                                 <div class="col-md-4">
-                                    <div class="m-2">
-                                        <div class="overflow-auto">
-                                            <div class="sticky-top">
-                                                <strong><?php echo $data['judulfoto'] ?></strong><br>
-                                                <span class="badge bg-secondary"><?php echo $data['namalengkap'] ?></span>
-                                                <span class="badge bg-secondary"><?php echo $data['tanggalunggah'] ?></span>
-                                                <span class="badge bg-primary"><?php echo $data['namaalbum'] ?></span>
-                                            </div>
-                                            <hr>
-                                            <p align="left">
-                                                <?php echo $data['deskripsifoto'] ?>
-                                            </p>
-                                            <hr>
-                                            <?php 
-                                            $fotoid = $data['fotoid'];
-                                            $komentar = mysqli_query($koneksi, "SELECT * FROM komentarfoto INNER JOIN user ON komentarfoto.userid=user.userid WHERE komentarfoto.fotoid='$fotoid'");
-                                            while($row = mysqli_fetch_array($komentar)){
-                                                ?>
-                                                <p align="left">
-                                                    <strong><?php echo $row['namalengkap'] ?></strong>
-                                                    <?php echo $row['isikomentar'] ?>
-                                                </p>
-                                            <?php } ?>
-                                            <hr>
-                                            <div class="sticky-bottom">
-                                                <form action="../config/proses_komentar.php" method="POST">
-                                                    <div class="input-group">
-                                                        <input type="hidden" name="fotoid" value="<?php echo $data['fotoid'] ?>">
-                                                        <input type="text" name="isikomentar" class="form-control" placeholder="Tambah Komentar">
-                                                        <div class="input-group-prepend">
-                                                            <button type="submit" name="kirimkomentar" class="btn btn-outline-primary">Kirim</button>
-                                                        </div>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-
+                                  <img src="../assets/img/<?php echo $data['lokasifile'] ?>" width="100">
                                 </div>
+                                <div class="col-md-8">
+                                  <label class="form-label">Ganti File</label>
+                                  <input type="file" class="form-control" name="lokasifile">
+                                </div>
+                              </div>
                             </div>
+                            <div class="modal-footer">
+                              <button type="submit" name="edit" class="btn btn-primary">Edit Data</button>
+                            </form>
+                          </div>
                         </div>
+                      </div>
                     </div>
-                </div>
-            </div>
 
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#hapus<?php echo $data['fotoid'] ?>">
+                      Hapus
+                    </button>
+
+                    <div class="modal fade" id="hapus<?php echo $data['fotoid'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      <div class="modal-dialog">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Hapus Data</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                            <form action="../config/aksi_foto.php" method="POST">
+                              <input type="hidden" name="fotoid" value="<?php echo $data['fotoid'] ?>">
+                              Apakah anda yakin akan menghapus data <strong> <?php echo $data['judulfoto'] ?> </strong> ?
+                            </div>
+                            <div class="modal-footer">
+                              <button type="submit" name="hapus" class="btn btn-primary">Hapus Data</button>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              <?php } ?>
+            </tbody>
+          </table>
         </div>
-    <?php } ?>
-</div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
-    <p>&copy;2024 Galeri Foto | Nur Azila</p>
+  <p>&copy; UKK RPL 2024 | nur azila</p>
 </footer>
 
 <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
 </body>
-
 </html>
