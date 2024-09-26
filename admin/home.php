bb33f52ba1eabcf1da713ee9fe0cf184513c7705
zil <azilanur965@gmail.com>
2024-09-26 08:22:41 +0800

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -1,29 +1,30 @@
-<?php
+<?php 
 session_start();
 $userid = $_SESSION['userid'];
-include '../config/koneksi.php';
 if ($_SESSION['status'] != 'login') {
   echo "<script>
-  alert('Anda belum login'); 
+  alert('Anda Belum Login!');
   location.href='../index.php';
   </script>";
 }
-?>
 
+include '../config/koneksi.php';
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
- <meta  charset="utf-8">
- <meta  name="viewport"  content="width=device-width, initial-scale=1">
- <title>Website Gareri Foto</title>
- <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Website Galeri Foto</title>
+  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
   <style>
     body {
-    background-image: url('../assets/img/bg3.jpg');
-    background-size: cover;}
-</style>
+      background-image: url('../assets/img/bg3.jpg');
+      background-size: cover;
+    }
+  </style>
 </head>
 <body>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -36,84 +37,80 @@ if ($_SESSION['status'] != 'login') {
         <div class="navbar-nav me-auto">
           <a href="home.php" class="nav-link">Home</a>
           <a href="album.php" class="nav-link">Album</a>
-          <a href="poto.php" class="nav-link">Foto</a>
+          <a href="foto.php" class="nav-link">Foto</a>
         </div>
-        <a href="../config/aksi_logout.php" class="btn btn-outline-danger m-1">keluar</a>
+        
+        <a href="../config/aksi_logout.php" class="btn btn-outline-danger m-1">Keluar</a>
       </div>
     </div>
   </nav>
 
   <div class="container mt-3">
     Album :
-    <?php
+    <?php 
     $album = mysqli_query($koneksi, "SELECT * FROM album WHERE userid='$userid'");
-    while($row = mysqli_fetch_array($album)){ ?>
-      <a href="home.php?albumid=<?php echo $row['albumid'] ?>" class="btn btn-outline-primary">
-        <?php echo $row['namaalbum'] ?></a>
+    while ($row = mysqli_fetch_array($album)) { ?>
+      <a href="home.php?albumid=<?php echo $row['albumid'] ?>" class="btn btn-outline-primary"><?php echo $row['namaalbum'] ?></a>
+    <?php } ?>
 
-      <?php } ?>
+    <div class="row">
+      <?php 
+      if (isset($_GET['albumid'])) {
+        $albumid = $_GET['albumid'];
+        $query = mysqli_query($koneksi, "SELECT * FROM foto WHERE userid='$userid' AND albumid='$albumid'");
+        while($data = mysqli_fetch_array($query)){ ?>
+          <div class="col-md-3 mt-2">
+            <div class="card">
+              <img style="height: 12rem;" src="../assets/img/<?php echo $data['lokasifile'] ?>" class="card-img-top" title="<?php echo $data['judulfoto'] ?>">
+              <div class="card-footer text-center">
+                <?php 
+                $fotoid = $data['fotoid'];
+                $ceksuka = mysqli_query($koneksi, "SELECT * FROM likefoto WHERE fotoid='$fotoid' AND userid='$userid'");
+                if (mysqli_num_rows($ceksuka) == 1) { ?>
+                  <a href="../config/proses_like.php?fotoid=<?php echo $data['fotoid'] ?>" type="submit" name="batalsuka"><i class="fa fa-heart"></i></a> 
+                <?php } else { ?>
+                  <a href="../config/proses_like.php?fotoid=<?php echo $data['fotoid'] ?>" type="submit" name="suka"><i class="fa-regular fa-heart"></i></a>
+                <?php }
+                $like = mysqli_query($koneksi, "SELECT * FROM likefoto WHERE fotoid='$fotoid'");
+                echo mysqli_num_rows($like) . ' suka';
+                ?>
+                <a href="#"><i class="fa-regular fa-comment"></i></a> 3 Komentar
+              </div>
+            </div>
+          </div>
 
-      <div class="row">
-        <?php
-        if (isset($_GET['albumid'])) {
-          $albumid = $_GET['albumid'];
-          $query = mysqli_query($koneksi, "SELECT * FROM foto WHERE userid='$userid' AND albumid='$albumid'");
-          while($data = mysqli_fetch_array($query)) {  ?>
-            <div class="col-md-3">
+        <?php } }else{
+
+          $query = mysqli_query($koneksi, "SELECT * FROM foto WHERE userid='$userid'");
+          while ($data = mysqli_fetch_array($query)) {
+            ?>
+            <div class="col-md-3 mt-2">
               <div class="card">
                 <img style="height: 12rem;" src="../assets/img/<?php echo $data['lokasifile'] ?>" class="card-img-top" title="<?php echo $data['judulfoto'] ?>">
                 <div class="card-footer text-center">
-
-                  <?php
+                  <?php 
                   $fotoid = $data['fotoid'];
                   $ceksuka = mysqli_query($koneksi, "SELECT * FROM likefoto WHERE fotoid='$fotoid' AND userid='$userid'");
                   if (mysqli_num_rows($ceksuka) == 1) { ?>
-                    <a href="../config/proses_like.php?fotoid=<?php echo $data['fotoid'] ?>" type="submit" name="batalsuka"><i class="fa fa-heart"></i></a>
-                  <?php }else{ ?>
+                    <a href="../config/proses_like.php?fotoid=<?php echo $data['fotoid'] ?>" type="submit" name="batalsuka"><i class="fa fa-heart"></i></a> 
+                  <?php } else { ?>
                     <a href="../config/proses_like.php?fotoid=<?php echo $data['fotoid'] ?>" type="submit" name="suka"><i class="fa-regular fa-heart"></i></a>
                   <?php }
                   $like = mysqli_query($koneksi, "SELECT * FROM likefoto WHERE fotoid='$fotoid'");
-                  echo mysqli_num_rows($like). ' Suka';
+                  echo mysqli_num_rows($like) . ' suka';
                   ?>
-                  <a href=""><i class="fa-regular fa-comment"></i></a>1 Komentar
-                </div>  
-              </div>
-            </div>
-
-          <?php } }else{ 
-
-            $query = mysqli_query($koneksi, "SELECT * FROM foto WHERE userid='$userid'");
-            while($data = mysqli_fetch_array($query)){
-              ?>
-              <div class="col-md-3 mt-2">
-                <div class="card">
-                  <img style="height: 12rem;" src="../assets/img/<?php echo $data['lokasifile'] ?>" class="card-img-top" title="<?php echo $data['judulfoto'] ?>">
-                  <div class="card-footer text-center">
-
-                    <?php
-                    $fotoid = $data['fotoid'];
-                    $ceksuka = mysqli_query($koneksi, "SELECT * FROM likefoto WHERE fotoid='$fotoid' AND userid='$userid'");
-                    if (mysqli_num_rows($ceksuka) == 1) { ?>
-                      <a href="../config/proses_like.php?fotoid=<?php echo $data['fotoid'] ?>" type="submit" name="batalsuka"><i class="fa fa-heart"></i></a>
-                    <?php }else{ ?>
-                      <a href="../config/proses_like.php?fotoid=<?php echo $data['fotoid'] ?>" type="submit" name="suka"><i class="fa-regular fa-heart"></i></a>
-                    <?php }
-                    $like = mysqli_query($koneksi, "SELECT * FROM likefoto WHERE fotoid='$fotoid'");
-                    echo mysqli_num_rows($like). ' Suka';
-                    ?>
-                    <a href=""><i class="fa-regular fa-comment"></i></a>3 Komentar
-                  </div>  
+                  <a href="#"><i class="fa-regular fa-comment"></i></a> 3 Komentar
                 </div>
               </div>
-            <?php } } ?>
-          </div>
+            </div>
+          <?php } }?>
         </div>
-        <footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
-          <p>&copy;2024 Galeri Foto | Nur Azila</p>
-        </footer>
+      </div>
 
+      <footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
+        <p>&copy; UKK RPL 2024 | nur azila</p>
+      </footer>
 
-
-        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-      </body>
-      </html
+      <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+    </body>
+    </html>
